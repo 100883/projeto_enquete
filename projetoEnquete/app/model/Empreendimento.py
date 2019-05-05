11ffbda92b20ddc8e4e87e96b40f08dc36f547d5
Guilherme Freitas <guifreitas89@live.com>
2019-05-05 10:34:43 -0300

    Correção na chamada da model Empreendimento

--- projetoEnquete/app/model/Empreendimento.py
+++ projetoEnquete/app/model/Empreendimento.py
@@ -2,7 +2,17 @@ from django.db import models
 from .Usuario import Usuario
 
 class Empreedimento(models.Model):
-    descricao = models.CharField(max_length=200)
+    EMPR_1 = 'le jardin'
+    EMPR_2 = 'evian'
+    EMPR_3 = 'olimpia thermas'
+
+    EMPR_CHOICES = (
+        (EMPR_1, "Le Jardin"),
+        (EMPR_2, "Evian"),
+        (EMPR_3, "Olimpia Thermas"),
+    )
+
+    descricao = models.CharField(max_length=20, choices=EMPR_CHOICES)
     foto_capa = models.ImageField(upload_to="images")
 
     def __str__(self):
--- projetoEnquete/app/model/Voto.py
+++ projetoEnquete/app/model/Voto.py
@@ -3,19 +3,8 @@ from .Usuario import Usuario
 from .Empreendimento import Empreedimento
 
 class Voto(models.Model):
-    EMPR_1 = 'le jardin'
-    EMPR_2 = 'evian'
-    EMPR_3 = 'olimpia thermas'
-
-    EMPR_CHOICES = (
-        (EMPR_1, "Le Jardin"),
-        (EMPR_2, "Evian"),
-        (EMPR_3, "Olimpia Thermas"),
-    )
-
     voto_user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    voto_empr = models.ForeignKey(Empreedimento, on_delete=models.CASCADE)
-    nome_empr = models.CharField(max_length=20, choices=EMPR_CHOICES)
+    nome_empr = models.CharField(max_length=20, choices=Empreedimento.EMPR_CHOICES)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.nome_empr
