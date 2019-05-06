296615510301213b84f3815c18b12992788dbe43
Guilherme Freitas <guifreitas89@live.com>
2019-05-06 15:29:18 -0300

    Ajustes

--- projetoEnquete/app/urls.py
+++ projetoEnquete/app/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     url(r'^empreendimento/listar/$', listar_empreendimento, name="listar_empreendimento"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
